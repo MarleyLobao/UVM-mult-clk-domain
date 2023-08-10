09e85be74f378e17310e4ec6b5d4cc0ca3bc8002
MarleyLobao <marley.sousa@ee.ufcg.edu.br>
2023-08-10 01:18:15 -0300

    Adds print_topology to show elements of the testbench

--- tb/src/base_test.sv
+++ tb/src/base_test.sv
@@ -11,4 +11,9 @@ class base_test extends uvm_test;
     super.build_phase(phase);
     env_h = env::type_id::create("env_h", this);
   endfunction
+
+  virtual function void start_of_simulation_phase(uvm_phase phase);
+    super.start_of_simulation_phase(phase);
+    uvm_top.print_topology();
+  endfunction
 endclass
